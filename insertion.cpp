2424319224afe20f0f690871816c650e4affdafa
seneerihab <seneerihab@gmail.com>
2026-02-12 11:29:28 +0530

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {-4, 6, 1, -9, 2};
+    int n = 5;
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array using Insertion Sort: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
